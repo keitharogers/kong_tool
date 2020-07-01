5f22880e617eecef6fe47b43d9536b9ef2000332
Keith Rogers <keith@Keiths-MacBook-Pro.local>
2020-07-01 11:52:45 +0100

    Adding logic for cert verification

--- kongrequests.py
+++ kongrequests.py
@@ -7,14 +7,23 @@ def make_request(method, api, params):
     config.read('config.ini')
 
     api_url = config['Kong']['API_URL']
+    api_url_verify_cert = config['Kong']['API_VERIFY_CERTIFICATE']
     url = api_url
 
     headers = {
         'cache-control': "no-cache"
     }
 
+    if api_url_verify_cert == 'false':
+        verify_cert = False
+    elif api_url_verify_cert == 'true':
+        verify_cert = True
+    else:
+        print("Please set API_URL_VERIFY_CERT to 'true' or 'false' in config.ini")
+        exit(1)
+
     if (method == 'POST') or (method == 'PATCH') or (method == 'PUT'):
-        resp = requests.request(method, url + api, headers=headers, data=params)
+        resp = requests.request(method, url + api, headers=headers, data=params, verify=verify_cert)
         if resp.status_code == 409:
             print('Object already exists please amend instead')
             exit(1)
@@ -26,10 +35,10 @@ def make_request(method, api, params):
             print('Request failed...')
             return resp.json()
     elif method == 'GET':
-        resp = requests.request(method, url + api, headers=headers, params=params)
+        resp = requests.request(method, url + api, headers=headers, params=params, verify=verify_cert)
         if resp.status_code == 200:
             return resp.json()
     elif method == 'DELETE':
-        resp = requests.request(method, url + api, headers=headers, data=params)
+        resp = requests.request(method, url + api, headers=headers, data=params, verify=verify_cert)
         if resp.status_code == 204:
             print('Deletion successful')
